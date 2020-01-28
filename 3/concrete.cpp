90ff89c7b51ba67bd1f1eea8eb3b6f0558e0dfd3
Mike <mshah.475@gmail.com>
2020-01-27 21:23:45 -0500

    Add files via upload

---
+++ 3/concrete.cpp
@@ -0,0 +1,28 @@
+class SpecificType1;
+class SpecificType2;
+class SpecificType3;
+
+
+int main(){
+
+	SpecificType1 object1 = new SpecificType1;
+	SpecificType2 object2 = new SpecificType2;
+	SpecificType3 object3 = new SpecificType3;
+
+	// Generate some object based on user input or data read in
+
+
+	// pseudo-code
+	if(typeid(myObject).name()=="SpecificType1"){
+
+	}
+	else if(typeid(myObject).name()=="SpecificType2"){
+
+	}
+	else if(typeid(myObject).name()=="SpecificType3"){
+
+	}
+
+}
+
+
---
+++ 3/creational.cpp
@@ -0,0 +1,14 @@
+class SpecificType1;
+class SpecificType2;
+class SpecificType3;
+
+
+int main(){
+
+	ValidType object1 = new ValidType;
+	ValidType object2 = new ValidType;
+	ValidType object3 = new ValidType;
+
+}
+
+
