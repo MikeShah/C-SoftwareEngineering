0779ba4d70b94966816200f57990194128a84bdb
Mike <mshah.475@gmail.com>
2020-09-13 23:27:23 -0400

    Add files via upload

--- 2/screensaver/src/main.cpp
+++ 2/screensaver/src/main.cpp
@@ -1,79 +1,34 @@
 // main.cpp 
+// Third-Party Library
 #include <SFML/Window.hpp>
 #include <SFML/Graphics.hpp>
+
+// Our includes
+#include "circle.hpp"
+
+// C++ Standard Template Library
+#include <vector>
+#include <ctime>
 
 constexpr int windowWidth{640};
 constexpr int windowHeight{480};
 
-class Circle{
-    public:
-        explicit Circle(bool up, bool left, float radius) : m_up(up),m_left(left),m_radius(radius) {
-            // Set the initial starting point
-            m_x = 5.0f;
-            m_y = 5.0f;
-            // Set the shapes radius
-            m_shape.setPosition(m_x,m_y);
-            m_shape.setRadius(m_radius);
-            m_shape.setFillColor(sf::Color::Blue);
-            m_shape.setOrigin(m_radius,m_radius);
-        }
-
-        ~Circle(){
-        }
-
-        void update(int xmax, int ymax){
-            if(m_up){
-                m_y-=1.0f;
-            }else{
-                m_y+=1.0f;
-            }
-            if(m_left){
-                m_x-=1.0f;
-            }else{
-                m_x+=1.0f;
-            }
-
-            if(m_x <0){
-                m_left=false;
-            }
-            if(m_x > xmax){
-                m_left=true;
-            }
-
-            if(m_y < 0){
-                m_up=false;
-            }
-            if(m_y > ymax){
-                m_up=true;
-            }
-
-            m_shape.setPosition(m_x,m_y);
-        }
-
-        void draw(sf::RenderWindow& rw){
-            rw.draw(m_shape);  
-        }
-
-    private:
-
-        sf::CircleShape m_shape;
-
-        float m_radius;
-        bool m_up;
-        bool m_left;
-        float m_x, m_y;
-};
-
 
 int main(){
-
+	// Set random number generator here
+	srand(time(NULL));
+	
     // Create a window to draw graphics on
     sf::RenderWindow window{sf::VideoMode(windowWidth, windowHeight,32), "Screen Saver"}; 
-    // Set the frame rate for how often teh window refreshes
+    // Set the frame rate for how often the window refreshes
     window.setFramerateLimit(60);
 
-    // Create a cirle shape
-    Circle myCircle(true,true,25.0f);
+    // Create a circle shape
+	std::vector<Circle> circles;
+	for(int i=0; i < 100; i++){
+		Circle temp;
+		circles.push_back(temp);
+	}
 
     // The main loop of an interactive graphics application.
     // Some applications are meant to run forever, so
@@ -95,8 +50,12 @@ int main(){
             }
         }
 
-        myCircle.update(640,480);
-        myCircle.draw(window);
+		for(int i=0; i < circles.size(); i++){
+			circles[i].update(640,480);
+			circles[i].draw(window);
+		}
+
+
         // Display the window
         window.display();
     }
