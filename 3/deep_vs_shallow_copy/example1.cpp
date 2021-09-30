8e998e1fab9a90b4294ea91b58c4fd17609afcf2
mike <mike@system76-pc.localdomain>
2021-09-30 16:56:57 -0400

    renamed some examples

---
+++ 3/deep_vs_shallow_copy/example1.cpp
@@ -0,0 +1,18 @@
+// @file example1.cpp
+
+class NewCollection{
+public:
+
+    int* m_array;   // Because this member variable
+                    // will be allocated as an array
+                    // we need to write our own constructor
+                    // and destructor
+};
+
+
+int main(){
+
+    return 0;
+}
+
+
---
+++ 3/deep_vs_shallow_copy/example2.cpp
@@ -0,0 +1,46 @@
+// @file example2.cpp
+// g++ -std=c++17 example2.cpp -o prog
+#include <iostream>
+
+class NewCollection{
+public:
+
+    int* m_array;   // Because this member variable
+                    // will be allocated as an array
+                    // we need to write our own constructor
+                    // and destructor
+};
+
+
+int main(){
+
+    NewCollection one;
+    // Allocate our array
+    // NOTE: If you only alloate say '10' ints
+    //       your program may not segfault.
+    //       processes are given 'some' amount of memory by default,
+    //       so the behavior is undefined.
+    one.m_array = new int[100000];
+    for(int i=0; i < 100000;i++){
+        one.m_array[i] = i;
+    }
+    // Here we are creating a copy of 'one'
+    // by assigning it to 'two'
+    NewCollection two = one;
+
+    // If we print out the addresses of m_array, we'll see
+    // a copy was created with different addresses.
+    // BUT -- the data we are pointing to is not the same
+    std::cout << "&one.m_array: " << &one.m_array << std::endl;
+    std::cout << "&two.m_array: " << &two.m_array << std::endl;
+
+    // Now we have two pointers pointing to the same data!
+    // We DID NOT get a true copy
+    for(int i=0; i < 100000;i++){
+        std::cout << "two.m_array[i] = " << two.m_array[i] << std::endl;
+    }
+
+    return 0;
+}
+
+
