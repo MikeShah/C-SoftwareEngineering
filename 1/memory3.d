03f42811917b545011fde97b6fcbe4fb20f1f33d
MikeShah <mshah.475@gmail.com>
2023-01-11 01:56:21 -0500

    d examples

---
+++ 1/memory3.d
@@ -0,0 +1,25 @@
+// @file memory3.d
+import std.stdio;
+
+void Safe() @safe{
+    string[] strings = new string[5];
+    writeln(strings);
+}
+
+void UnSafe() @system // Note: @system is the default
+{                     // so you don't have to label
+    int* p = new int;
+    // Pointer arithemtic generally is
+    // not 'safe'
+
+    // TRY changing @system to @safe here, and
+    //     this will not compile
+    p = p + 1;
+}
+
+void main(){
+    Safe();
+    UnSafe();
+
+}
+
