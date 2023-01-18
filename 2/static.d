268e7f8cb2db94d948ded8f072b35db19e49e4b2
MikeShah <mshah.475@gmail.com>
2023-01-18 02:56:55 -0500

    static

---
+++ 2/static.d
@@ -0,0 +1,17 @@
+// @file static.d
+import std.stdio;
+void foo(){
+    // 
+    static i =0; // equivalent to:
+                 // static int i=0; OR
+                 // static auto i=0;
+
+    i++;
+    writeln(i);
+}
+
+void main(){
+    foo();
+    foo();
+    foo();
+}
