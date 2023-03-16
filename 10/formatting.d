d9ed534a0f12cbb0113d88825c0ba95131a41033
mike <mike@system76-pc.localdomain>
2023-03-15 20:40:35 -0400

    added comment

--- 10/formatting.d
+++ 10/formatting.d
@@ -1,10 +1,12 @@
+// dub run dfmt -- --inplace --space_after_cast=false --max_line_length=80 --soft_max_line_length=70 --brace_style=otbs formatting.d
 void main(string[] args) {
     bool optionOne, optionTwo, optionThree;
 
     // dfmt has no way of knowing that 
     // "getopt" is special, so it wraps the
     // argument list normally
-    getopt(args, "optionOne", &optionOne, "optionTwo", &optionTwo, "optionThree", &optionThree);
+    getopt(args, "optionOne", &optionOne, "optionTwo", &optionTwo,
+            "optionThree", &optionThree);
 
     // dfmt off
     getopt(args,
