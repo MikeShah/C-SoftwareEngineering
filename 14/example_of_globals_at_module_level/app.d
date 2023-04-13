63ba44f1b8b039534aea7dbadb5401fb7b3c011e
mike <mike@system76-pc.localdomain>
2023-04-13 17:37:31 -0400

    example

---
+++ 14/example_of_globals_at_module_level/app.d
@@ -0,0 +1,14 @@
+import std.stdio;
+import gui;
+
+void main(){
+				writeln(gui.red);
+				writeln(gui.green);
+				writeln(gui.blue);
+
+				gui.saveColors();
+
+				writeln(gui.red);
+				writeln(gui.green);
+				writeln(gui.blue);
+}
