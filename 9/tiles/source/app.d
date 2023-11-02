11703132b7155c850c97d911c2566f8b5fac41f1
mike <mike@system76-pc.localdomain>
2023-11-02 17:39:08 -0400

    quick refacttor

--- 9/tiles/source/app.d
+++ 9/tiles/source/app.d
@@ -9,47 +9,11 @@ import std.string;
 //import setup_sdl;
 import sprite;
 import tilemap;
+import player;
 
 // Load the SDL2 library
 import bindbc.sdl;
 
-struct Player{
-    // Load our sprite
-    Sprite mSprite;
-
-    this(SDL_Renderer* renderer, string filepath){
-        mSprite = Sprite(renderer,filepath);
-    }
-
-    int GetX(){
-        return mSprite.mXPos;
-    }
-    int GetY(){
-        return mSprite.mYPos;
-    }
-
-    void MoveUp(){
-        mSprite.mYPos -=16;
-        mSprite.mState = STATE.WALK;
-    }
-    void MoveDown(){
-        mSprite.mYPos +=16;
-        mSprite.mState = STATE.WALK;
-    }
-    void MoveLeft(){
-        mSprite.mXPos -=16;
-        mSprite.mState = STATE.WALK;
-    }
-    void MoveRight(){
-        mSprite.mXPos +=16;
-        mSprite.mState = STATE.WALK;
-    }
-
-    void Render(SDL_Renderer* renderer){
-        mSprite.Render(renderer);
-        mSprite.mState = STATE.IDLE;
-    }
-}
 
 interface Command{
     void Execute();
